38d23e32a0809cbce8a59585b61f6da556879a05
godly-AK <adarsh.krishn5447@gmail.com>
2026-03-28 19:17:57 +0000

    V1.0 pre final verison

---
+++ app/src/main/java/com/example/routineai/CategoryScoreResult.java
@@ -0,0 +1,9 @@
+package com.example.routineai;
+
+public class CategoryScoreResult {
+    public String categoryName;
+    public int totalTasks;
+    public int tasksCompleted;
+    public int tasksMissed;
+    // We will calculate the final percentage score in Java later!
+}
